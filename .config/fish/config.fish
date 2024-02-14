6a1cb56a5637faacd81e7990e02fc0bc82ed6eee
chumeng <2595248810@qq.com>
2024-02-14 18:26:24 +0800

    edit fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,16 @@
 set -gx PATH "$HOME/.cargo/bin" $PATH
 set -gx EDITOR nvim
 
+function proxy
+	set -xg hostip $(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+	set -xg wslip $(hostname -I | awk '{print $1}')
+	set -xg port 7890
+	set -xg PROXY_HTTP "http://$hostip:$port"
+	set -xg ALL_PROXY $PROXY_HTTP
+	set -xg HTTPS_PROXY $PROXY_HTTP
+	set -xg HTTP_PROXY $PROXY_HTTP
+end
+
 starship init fish | source
 zoxide init fish | source
 set fish_greeting
@@ -8,6 +18,7 @@ set fish_greeting
 alias cat="bat"
 alias find="fd"
 alias ls='exa --git --icons --color=always --group-directories-first'
+alias la='ls -la'
 alias cp="xcp"
 alias grep="rg"
 alias cd="z"
