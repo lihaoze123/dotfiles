a81f368e3d8980663924404a10e1301f5fad7259
chumeng <2595248810@qq.com>
2024-02-21 21:51:13 +0800

    edit fish function cross-compile-rust-git

--- .config/fish/functions/cross-compile-rust-git.fish
+++ .config/fish/functions/cross-compile-rust-git.fish
@@ -1,18 +1,23 @@
 function cross-compile-rust-git
-      set target (echo $argv | awk -F '/' '{print $NF}')
-      git clone --depth=1 $argv
-      cd $target
-      set binary (
-            set tmp (taplo get -f Cargo.toml 'bin[0].name' 2> /dev/null)
+    set target (echo $argv | awk -F '/' '{print $NF}')
+    git clone --depth=1 $argv
+    cd $target
+    set binary (
+          set tmp (taplo get -f Cargo.toml 'bin[0].name' 2> /dev/null)
             if test $tmp
                 echo $tmp
             else
                 echo $target
             end
       )
-      cp -r ~/download/.cargo ~/download/$target
-      cargo build --release --locked
-      cp target/armv7-unknown-linux-gnueabihf/release/$binary ~/compiled/
-      cd ..
-      rm -rf $target                                                                                                                                                              
-  end
+    cp -r ~/download/.cargo ~/download/$target
+
+    if test -e rust-toolchain.toml
+        rm rust-toolchain.toml
+    end
+
+    cargo build --release --locked
+    cp target/armv7-unknown-linux-gnueabihf/release/$binary ~/compiled/
+    cd ..
+    rm -rf $target                                                                                                                                                              
+end
