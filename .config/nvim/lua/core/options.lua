f725906c289d8a27a861822defd79ea00e8f592c
chumeng <2595248810@qq.com>
2024-07-18 20:20:16 +0800

    update nvim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -20,25 +20,3 @@ opt.smartcase = true
 -- 外观
 opt.signcolumn = "yes"
 vim.cmd[[colorscheme dracula]]
-
--- Auto Completion
-opt.backup = false
-opt.writebackup = false
-opt.updatetime = 300
-opt.signcolumn = "yes"
-
-local keyset = vim.keymap.set
-
-function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-
-vim.g.coc_snippet_next = '<TAB>'
-vim.g.coc_snippet_prev = '<S-TAB>'
-
-keyset("i", "<TAB>", [[coc#pum#visible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" : v:lua.check_back_space() ? "\<TAB>" : coc#refresh()]], opts)
-keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
